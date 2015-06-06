aebe37d9650eefdd34ff64246042ec587246ad7a
TieSoul <tdbaksteen@hotmail.nl>
2015-06-06 14:47:42 +0200

    Added do_stuff.js

---
+++ do_stuff.js
@@ -0,0 +1,49 @@
+/*
+ main_out quick reference:
+ A = ja = document.getElementById("canvas");
+ e = A.getContext("2d");
+ S = mouse.x;
+ T = mouse.y;
+ Na() - Initialize?
+
+ */
+
+tester = document.getElementsByTagName("script");
+discovered_mainouturl = 0;
+
+for (i=0; i<tester.length; i++ ){
+    src = tester[i].src;
+    if (src.substring(0, main_out_url.length ) == main_out_url) {
+        discovered_mainouturl = src.replace("http://agar.io/","");
+    }
+}
+
+if(discovered_mainouturl != 0) {
+    httpGet(discovered_mainouturl, function(data) {
+        gamejs = data;
+        gamejs = gamejs.replace(/\n/g, "");
+        injectBot();
+    })
+}
+
+function httpGet(theUrl, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, true);
+    xmlHttp.send(null);
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            callback(xmlHttp.responseText);
+        }
+    };
+}
+
+function injectBot() {
+    var script = document.createElement("script");
+    script.src='//cdnjs.cloudflare.com/ajax/libs/canvasjs/1.4.1/canvas.min.js';
+    document.head.appendChild(script);
+
+    gamejs = gamejs.replace("wa(){", "wa(){console.log('test');");
+    script = document.createElement("script");
+    script.innerHTML = gamejs;
+    document.head.appendChild(script);
+}
